518638b134b3ef218f5aebc9468cf6b55cf358cb
crhan <crhan123@gmail.com>
2012-04-20 22:04:32 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrhanGit::Application.config.secret_token = '05d4a5f18b62d6495fdcc794b14112338a4882528a2a7521e294a509eba24a364a096766fbb0362c8ff5293a1b5740055e52c6fe6aaab9e7eef89cd5ca6503d2'
