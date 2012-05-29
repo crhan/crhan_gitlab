686cfb2e3b2cc5baeef449b1c7019166919a1d8d
crhan <crhan123@gmail.com>
2012-05-29 14:50:07 +0800

    destroy user itself

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,14 @@
 class ProfilesController < ApplicationController
   before_filter :find_profile
   before_filter :authenticate_user!
+  respond_to :html
 
   def show
+  end
 
+  def destroy
+    @profile.destory
+    respond_with(@profile)
   end
 
   private
